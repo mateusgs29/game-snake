634ebad0fa079a2c44251941d005db2b15aed009
Mateus Gomes <mateus.gs1999@gmail.com>
2020-12-03 19:16:24 -0300

    jogo base completo

--- js/script.js
+++ js/script.js
@@ -7,6 +7,10 @@ snake[0] = {
     y: 8 * box
 }
 let direction = "right";
+let comida = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 //funcao para criar o background
 function criarBG(){
@@ -23,9 +27,36 @@ function criarCobrinha(){
     }
 }
 
+function criarComida(){
+    context.fillStyle = "red";
+    context.fillRect(comida.x, comida.y, box, box);
+}
+
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != "right") direction = "left"; 
+    if(event.keyCode == 38 && direction != "down") direction = "up"; 
+    if(event.keyCode == 39 && direction != "left") direction = "right"; 
+    if(event.keyCode == 40 && direction != "up") direction = "down";  
+}
+
 function iniciarJogo(){
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over');
+        }
+    }
+
     criarBG();
     criarCobrinha();
+    criarComida();
 
     let snake_X = snake[0].x; 
     let snake_Y = snake[0].y;
@@ -35,7 +66,13 @@ function iniciarJogo(){
     if(direction == "up") snake_Y -= box;
     if(direction == "down") snake_Y += box;
 
-    snake.pop();
+    if(snake_X != comida.x || snake_Y != comida.y){
+        snake.pop();
+
+    }else{
+        comida.x = Math.floor(Math.random() * 15 + 1) * box;
+        comida.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x: snake_X,
